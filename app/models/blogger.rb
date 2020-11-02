b98f417a721cdfc0fe5e2e1ab2fc4fea743c7bdc
Jaquan1314 <javaughngrant@gmail.com>
2020-11-02 18:32:43 -0500

    Done w/ Shevaughn

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,18 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    validates :name, uniqueness: true
+    validates :age, numericality: { greater_than: 0 }
+    validates :bio, length: { minimum: 30,
+        too_short: "%{30} characters is the minimum allowed" 
+    }
+
+    def total_likes
+        self.posts.pluck(:likes).sum
+    end
+
+    def top_post
+        self.posts.max_by { |post| post.likes }
+    end
 end
